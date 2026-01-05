4af94e31ee2381df94f5d4cc0bb1db4d1f132d69
bitbytebd <mous4422@gmail.com>
2026-01-05 17:30:24 +0600

    file added

---
+++ src/jan05/Person.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jan05;
+
+/**
+ *
+ * @author Tultuli
+ */
+public class Person {
+    private int id;
+    private String name;
+    private double salary;
+
+    Person() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Person(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + ", salary=" + salary + '}';
+    }
+    
+    
+}
---
+++ src/jan05/Test.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jan05;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+/**
+ *
+ * @author Tultuli
+ */
+public class Test {
+    public static void main(String[] args) {
+        Person p1 = new Person(1001, "MR.A", 55000);
+        Person p2 = new Person(1002, "MR.B", 58500);
+        Person p3 = new Person(1003, "MR.C", 65000);
+
+     List <Person> personList = new ArrayList<Person>();
+            personList.add(p1);
+            personList.add(p2);
+            personList.add(p3);
+            
+            
+             
+             
+           System.out.println(personList);
+          
+         for(Person obj : personList){
+               System.out.println(obj);
+           }  
+      
+       
+         
+       }
+    }
